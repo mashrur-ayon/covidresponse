4fd6acaae300608494946ea12edd08aa64c6305f
mashrur ayon <mashrur399@gmail.com>
2021-07-13 23:17:41 +0600

    update of the main plot

--- covidresponse.R
+++ covidresponse.R
@@ -15,11 +15,22 @@ glimpse(covid_data)
 #Checking all the variables
 names(covid_data)
 
-corelation_data <- select(covid_data, Econocmi_Crisis_During_Pandemic
-, No_Economic_Crisis_During_Pandemic)
+corelation_data <- select(covid_data, Economic_Crisis
+, Oppression)
 
+cor(corelation_data)
 
-ggcorr(corelation_data)
+ggplot(corelation_data, aes(x= Economic_Crisis, y= Oppression)) + geom_point() + 
+  geom_smooth(formula = y ~ poly(x,2), method = "lm", se = T, level= 0.9) + 
+  geom_abline(aes(intercept=0, slope=2), color="red", linetype=6) +
+  labs(
+    x = "Reported Economic Crisis", 
+    y = "Reported Domestic Oppression", 
+    title = "Relationship between reported economic crisis, 
+and domestic oppression",
+    subtitle = "r= 0.982"
+  )
+
 plot<- ggpairs(corelation_data)
 
 #Plotting the results
